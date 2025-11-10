af275768685fe579b2a7549e74d018a05579b91f
vekkul1 <veeti.hallaaho@gmail.com>
2025-11-10 14:10:45 +0200

    exc.9

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -4,17 +4,17 @@ from player_stats import PlayerStats
 #from player_to_table import player_to_table
 
 def main():
-    c = Console()
-    s = c.input("S [bright blue] (2024-25) [/]")
-    u=f"https://studies.cs.helsinki.fi/nhlstats/{s if s else "2024-25"}/players"
-    reader = PlayerReader(u)
+    console = Console()
+    season = console.input("Season [bright blue] (2024-25) [/]")
+    url=f"https://studies.cs.helsinki.fi/nhlstats/{season if season else "2024-25"}/players"
+    reader = PlayerReader(url)
     stats = PlayerStats(reader)
 
     while True:
         country = input(f"Nationality [{"/".join(stats.get_nationalities())}] ")
 
         if country.upper() in stats.get_nationalities():
-            c.print(reader.players_to_table(stats.nationality(country), s))
+            console.print(reader.players_to_table(stats.nationality(country), season))
         else:
             return
 
