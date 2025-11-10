39c5482f02cc8ae9be7671593d91f293459be34b
vekkul1 <veeti.hallaaho@gmail.com>
2025-11-10 20:04:34 +0200

    index work

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 from logger import logger
+from summa import summa
+from erotus import erotus
 
 logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
-=======
-x = int(input("luku 1: "))
-y = int(input("luku 2: "))
->>>>>>> laskut
