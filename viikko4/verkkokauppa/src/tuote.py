232a41b92152a1b472735eb3aab3300c4dda61e3
vekkul1 <veeti.hallaaho@gmail.com>
2025-11-24 23:10:40 +0200

    feat: importance toggle for tuote!

--- viikko4/verkkokauppa/src/tuote.py
+++ viikko4/verkkokauppa/src/tuote.py
@@ -1,8 +1,9 @@
 class Tuote:
-    def __init__(self, id, nimi, hinta):
+    def __init__(self, id, nimi, hinta, important = False):
         self.id = id
         self.nimi = nimi
         self.hinta = hinta
+        self.important = important
 
     def __hash__(self):
         return hash(self.id)
@@ -12,3 +13,6 @@ class Tuote:
 
     def __str__(self):
         return self.nimi
+    
+    def toggle_important(self):
+        self.important = !self.important
