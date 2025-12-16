dcdfc6f9f908812fdf5a557d740f0b084ca84fa4
vekkul1 <veeti.hallaaho@gmail.com>
2025-12-16 13:31:52 +0200

    viikko6

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,29 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import *
+
+def main():
+    url = "https://studies.cs.helsinki.fi//nhlstats/2024-25/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    query = QueryBuilder()
+    
+    matcher = (
+        query
+            .one_of(
+            query.plays_in("PHI")
+                .has_at_least(10, "assists")
+                .has_fewer_than(10, "goals"),
+            query.plays_in("EDM")
+                .has_at_least(50, "points")
+            )
+            .build()
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
